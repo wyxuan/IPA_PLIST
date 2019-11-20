041a37aec469acbc84fbc8412ef587889eef61c1
wyxuan <w_yongxuan@163.com>
2019-11-20 17:33:17 +0800

    LBOC_Teacher

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 set -e
 
 ipaName=$1
+newVersion=$2
 plistHref=""
 fileName=""
 i=0
@@ -10,13 +11,13 @@ do
    current_time=`ls -l -h build.sh | awk '{print $6, $7, $8}'`
    fileSize=`ls -l -h build.sh | awk '{print $5}'`
    build_version=${fileName:0:11}
-   if [[ ${fileName:0:11} == "student_ipa" ]] && [ ${i} -lt 4 ];
+   if [[ ${fileName:0:11} == ${ipaName}"_ipa" ]] && [ ${i} -lt 4 ];
    then
      add='<center><br><br><br><h1>乐播课</h1><br><h2>>>> <a href="itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/wyxuan/IPA_PLIST/master/'$fileName'.plist">点击下载</a > <<<</h2><h6>版本：'${fileName}'</h6><h6>文件大小：'${fileSize}'</h6><h6>更新时间：'${current_time}'</h6></center>'
      plistHref=${plistHref}${add}
    fi
 done
-cat << EOF > student.html
+cat << EOF > ${ipaName}.html
  <!DOCTYPE html>
  <html>
  <head>
@@ -33,8 +34,12 @@ cat << EOF > student.html
  </html>
 EOF
 
-#plist
-cat << EOF > student.plist
+
+#plist UB_Student LBOC_Teacher
+
+if [[ ${ipaName} == "LBOC_Teacher" ]];
+then
+cat << EOF > LBOC_Teacher_${newVersion}.plist
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
@@ -48,7 +53,41 @@ cat << EOF > student.plist
                         <key>kind</key>
                         <string>software-package</string>
                         <key>url</key>
-                        <string>http://10.240.15.83/$ipaName.ipa</string>
+                    <string>http://10.240.15.83/LBOC_Teacher_${newVersion}.ipa</string>
+                    </dict>
+                </array>
+                <key>metadata</key>
+                <dict>
+                    <key>title</key>
+                    <string>乐•Adhoc</string>
+                    <key>bundle-version</key>
+                    <string>1</string>
+                    <key>kind</key>
+                    <string>software</string>
+                    <key>bundle-identifier</key>
+                    <string>com.dfyb.test.LBOCTeacher</string>
+                </dict>
+            </dict>
+        </array>
+    </dict>
+    </plist>
+EOF
+else
+cat << EOF > UB_Student_${newVersion}.plist
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+        <key>items</key>
+        <array>
+            <dict>
+                <key>assets</key>
+                <array>
+                    <dict>
+                        <key>kind</key>
+                        <string>software-package</string>
+                        <key>url</key>
+                    <string>http://10.240.15.83/UB_Student_${newVersion}.ipa</string>
                     </dict>
                 </array>
                 <key>metadata</key>
@@ -67,7 +106,7 @@ cat << EOF > student.plist
     </dict>
     </plist>
 EOF
-
+fi
 
 git add .
 git commit -m "${ipaName}"
