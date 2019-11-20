e270aa7b146d2316ac9e02860b61adcdfc284d56
wyxuan <w_yongxuan@163.com>
2019-11-20 18:25:39 +0800

    6.3.2

--- build.sh
+++ build.sh
@@ -5,13 +5,16 @@ newVersion=$2
 plistHref=""
 fileName=""
 i=0
+
+#if [[ $A == *$B* ]]
+echo "app: ${ipaName} v:  ${newVersion}"
 for file in `ls -l -h | awk '{print $9}'`
 do
    fileName=${file}
    current_time=`ls -l -h build.sh | awk '{print $6, $7, $8}'`
    fileSize=`ls -l -h build.sh | awk '{print $5}'`
    build_version=${fileName:0:11}
-   if [[ ${fileName:0:11} == ${ipaName}"_ipa" ]] && [ ${i} -lt 4 ];
+   if [[ ${fileName} == ${ipaName}"_"*.ipa ]] && [ ${i} -lt 4 ];
    then
      add='<center><br><br><br><h1>乐播课</h1><br><h2>>>> <a href="itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/wyxuan/IPA_PLIST/master/'$fileName'.plist">点击下载</a > <<<</h2><h6>版本：'${fileName}'</h6><h6>文件大小：'${fileSize}'</h6><h6>更新时间：'${current_time}'</h6></center>'
      plistHref=${plistHref}${add}
@@ -108,6 +111,8 @@ cat << EOF > UB_Student_${newVersion}.plist
 EOF
 fi
 
-git add .
-git commit -m "${ipaName}"
-git push origin master
+cp ${ipaName}_${newVersion}.plist /Users/dfub/.jenkins/workspace/IPA_PLIST/UB_Student_${newVersion}.plist
+
+#git add .
+#git commit -m "${ipaName}"
+#git push origin master
